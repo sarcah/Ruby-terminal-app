c387b29247fbaaf7a662f077a10bc2fe5f21b88a
sarahcahill <sjcahill03@gmail.com>
2021-04-02 18:43:05 +1100

    adding final question

--- quiz.rb
+++ quiz.rb
@@ -1,6 +1,6 @@
 require "tty-prompt"
 
-
+# Question class
 class Question
     attr_accessor :prompt, :answer
     def initialize (prompt, answer)
@@ -9,6 +9,7 @@ class Question
     end
 end
 
+# Question list
 p1 = "\n'I have never seen a thin person drinking Diet Coke.' Who said it? \n\nTrump       Kanye"
 p2 = "\n'For anyone that has money they know the first rule is to use other people's money.' Who said it? \nTrump       Kanye"
 
@@ -17,6 +18,7 @@ questions = [
     Question.new(p2, "Kanye")
 ]
 
+# Intro text
 pastel = Pastel.new
 
 puts pastel.cyan("What's your name?")
@@ -24,6 +26,7 @@ answer = gets.chomp
 puts pastel.yellow ("\nHi #{answer}! Let's see how well you know your Trump and Kayne.\n")
 puts pastel.yellow.bold ("Tell me who tweeted the following.\n")
 
+# Runs questions
 def run_quiz (questions)
     pastel = Pastel.new
     answer = ""
@@ -40,6 +43,7 @@ def run_quiz (questions)
 end
 
 
+
 # prompt = TTY::Prompt.new
 # greeting = 'What is the capital of Scotland?'
 # choices = %w(Edinburgh Glasgow)
@@ -47,17 +51,41 @@ end
 # 'do something' if answer == choices[0]
 # end
     
-# Final score    
-puts ( "\nThat's the end of the quiz questions. You got " + score.to_s + "/" + questions.length().to_s )
+# Total score question  
+pastel = Pastel.new
+puts pastel.magenta( "\nThat's the end of the quiz questions. You got " + score.to_s + "/" + questions.length().to_s )
     if score > questions.length / 2
-        puts "😄Great!😄"
+        puts "Great! 👏"
     elsif score == 0
-        puts "Dude.." 
+        puts "Dude... 😳" 
     else
-        puts "Keep working on it." 
+        puts "Keep working on it 👍" 
+    end
+
+    # gamble question
+    puts ( "\nYour total is " + score.to_s + ". Would you like to gamble these points?\nIf you're right, you'll double your points.\nIf your wrong, you'll lose everything...\nType Yes or No" )
+    answer_gamble = gets.chomp
+    if answer_gamble = "Yes"
+        puts "\nWho has more followers? Trump or Kanye?"
+        answer_final = gets.chomp
+            if answer_final == "Trump"
+                puts "\nNope! He no longer has a Twitter account, remember?"
+                score = 0
+                puts pastel.magenta "Your final score is 0"
+            elsif answer_final == "Kanye"
+                puts "\nYep! Trump no longer has a Twitter account, so has no followers"
+                score = score * 2
+                puts pastel.magenta ("Your final score is " + score.to_s )
+                # put error message in here?
+            end
+    else puts "👋"
     end
 end
 
 
 
+
 run_quiz (questions)
+
+
+
