85da7f8600e9506f9dc629b8c96204e6bf38f6e2
sarahcahill <sjcahill03@gmail.com>
2021-04-09 16:37:29 +1000

    final readme doc and organise files

--- spec/quiz_spec.rb
+++ spec/quiz_spec.rb
@@ -1,21 +1,17 @@
 require 'rspec'
-# require_relative "../quiz_class.rb"
-require "../quiz_class.rb"
+require "../src/quiz_class.rb"
 
-# describe "test" do
-#     it 'should print test' do
-#         expect test.to eq "Hello World"
-#     end
-# end
 
 RSpec.describe Question do
     it { is_expected.to respond_to :answer }
     it { is_expected.to respond_to :prompt }
 end
 
-RSpec.describe "Question" do
+RSpec.describe "Question" doc
     subject { Question.new }
     it "should respond to name" do
-        expect(subject).to respond_to :name
+        expect(subject).to respond_to :answer
     end
 end
+
+puts "yo"
