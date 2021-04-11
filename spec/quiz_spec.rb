ee42554188bc789709aac630a6b23a75d0d900b6
sarahcahill <sjcahill03@gmail.com>
2021-04-11 15:16:26 +1000

    final upload

--- spec/quiz_spec.rb
+++ spec/quiz_spec.rb
@@ -1,3 +1,4 @@
+
 require 'rspec'
 require "../src/quiz_class.rb"
 
@@ -7,11 +8,10 @@ RSpec.describe Question do
     it { is_expected.to respond_to :prompt }
 end
 
-RSpec.describe "Question" doc
+RSpec.describe "Question" do
     subject { Question.new }
     it "should respond to name" do
         expect(subject).to respond_to :answer
     end
 end
 
-puts "yo"
